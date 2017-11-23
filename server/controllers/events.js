a3ee51cbe1e1f1d1ee456daeb6909b921da3827b
Okereke Onyekachi <robocopkaka@gmail.com>
2017-11-23 23:48:57 +0100

    Chore: Write tests to check for invalid IDs and malformed requests. Feat: Write code to pass tests.

--- server/controllers/events.js
+++ server/controllers/events.js
@@ -9,7 +9,7 @@ const events = [
   {
     id: '2',
     name: 'Kachi\'s event',
-    date: '2011-01-01',
+    date: '2011-01-02',
     time: '08:00',
     centerId: 1
   }
@@ -17,9 +17,15 @@ const events = [
 
 module.exports = {
   edit(req, res) {
-    console.log(req.params);
     const event = events.find(anEvent => anEvent.id === req.params.eventId);
-    event.name = req.body.name;
-    res.status(200).send(event);
+    if (event === undefined) {
+      res.status(404).send('Resource not found');
+    } else if (req.body.name === undefined || req.body.date === undefined
+      || req.body.time === undefined || req.body.centerId === undefined) {
+      res.status(400).send('Bad request');
+    } else {
+      event.name = req.body.name;
+      res.status(200).send(event);
+    }
   }
 };
--- server/test/eventsTest.js
+++ server/test/eventsTest.js
@@ -24,4 +24,51 @@ describe('PUT /events/<eventid>', () => {
         err.should.have.status(400);
       });
   });
+  it('should return resource not found if the id doesn\'t exist yet', () => {
+    chai.request(app)
+      .put('/events/10')
+      .send({
+        name: 'edited name',
+        date: '2017-11-11',
+        time: '08:00',
+        centerId: 3
+      })
+      .then(() => {
+        // res.should.have.status(404);
+        // res.body.should.eql('rsr');
+      })
+      .catch((err) => {
+        err.should.have.status(404);
+      });
+  });
+  it('should not edit an event if the parameters supplied are invalid', () => {
+    chai.request(app)
+      .put('/events/1')
+      .send({
+        name: undefined,
+        date: undefined,
+        time: undefined,
+        centerId: undefined
+      })
+      .then(() => {
+        //
+      })
+      .catch((err) => {
+        err.should.have.status(400);
+      });
+  });
+  it('should not edit an event if a malformed request is passed', () => {
+    chai.request(app)
+      .put('/events/1')
+      .send({
+        hack: 'yeah',
+        some: 'stuff'
+      })
+      .then(() => {
+        //
+      })
+      .catch((err) => {
+        err.should.have.status(400);
+      });
+  });
 });
