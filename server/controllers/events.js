9d8e5f2cd4b2c03dfc0b78e2f1ba0578df5d7ee6
Okereke Onyekachi <robocopkaka@gmail.com>
2017-11-24 11:56:17 +0100

    Chore: Write test to handle when a user tries deleting an event with an invalid id. Fix: Resolve issue where events aren't being deleted properly.

--- server/controllers/events.js
+++ server/controllers/events.js
@@ -1,34 +1,40 @@
 const events = [
   {
-    id: 1,
+    id: '1',
     name: 'kachi\'s event',
     date: '2011-11-11',
     time: '08:00',
-    centerId: 1
+    centerId: '1'
   },
   {
-    id: 3,
+    id: '2',
     name: 'kachi\'s second event',
     date: '2011-11-11',
     time: '08:00',
-    centerId: 1
+    centerId: '3'
   },
   {
-    id: 3,
+    id: '3',
     name: 'kachi\'s event',
     date: '2011-11-11',
     time: '08:00',
-    centerId: 1
+    centerId: '2'
   }
 ];
 
 module.exports = {
   delete(req, res) {
-    const event = events.find(anEvent => anEvent.id === req.params.id);
-    events.splice(event, 1);
-    res.status(200).send({
-      message: 'Resource deleted successfully',
-      data: event
-    });
+    const event = events.find(anEvent => anEvent.id === req.params.eventId);
+    if (event === undefined) {
+      res.status(404).send({
+        message: 'Resource not found'
+      });
+    } else {
+      const eventId = events.findIndex(anEvent => anEvent.id === req.params.eventId);
+      events.splice(eventId, 1);
+      res.status(200).send({
+        message: 'Resource deleted successfully'
+      });
+    }
   }
 };
