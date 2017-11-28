be6c509cf20b56f969634717ae439ea5e01a863f
Okereke Onyekachi <robocopkaka@gmail.com>
2017-11-28 15:44:18 +0100

    Remove the return statement in the migration file for creating centers

--- server/migrations/20171128122725-create-center.js
+++ server/migrations/20171128122725-create-center.js
@@ -1,6 +1,6 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Centers', {
+    queryInterface.createTable('Centers', {
       id: {
         allowNull: false,
         autoIncrement: true,
