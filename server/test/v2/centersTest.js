77066ed8c7bc19e64cbfbde4f374c724488961ff
Okereke Onyekachi <robocopkaka@gmail.com>
2017-12-01 17:15:04 +0100

    Write tests for editing centers

--- server/test/v2/centersTest.js
+++ server/test/v2/centersTest.js
@@ -71,3 +71,52 @@ describe('GET /api/v2/centers/<centerId>', () => {
       });
   });
 });
+
+describe('PUT /api/v1/centers/<centerId>', () => {
+  it('should return a 200 if the parameters were valid', () => {
+    chai.request(app)
+      .put('/api/v2/centers/1')
+      .send({
+        name: 'The main centerssssssss',
+        detail: 'We exist',
+        image: 'ramsey.jpg',
+        address: 'somewhere in lagos',
+        state: 'lagos',
+        capacity: 1000
+      })
+      .then((res) => {
+        res.should.have.status(200);
+        res.body.data.should.have.property('name').eql('The main centerssssssss');
+      });
+  });
+  it('should return a 400 if the parameters were invalid', () => {
+    chai.request(app)
+      .put('/api/v2/centers/1')
+      .send({})
+      .catch((err) => {
+        err.should.have.status(400);
+      });
+  });
+  it('should return a 409 if there\'s a resource conflict with center names', () => {
+    chai.request(app)
+      .put('/api/v2/centers/1')
+      .send(newCenterDB)
+      .then(() => {
+        // res.should.have.status(409);
+      })
+      .catch((err) => {
+        err.should.have.status(409);
+        err.response.error.text.should.eql('Resource conflict');
+      });
+  });
+  it('should return a 404 if the ID is invalid or doesn\'t exist', () => {
+    chai.request(app)
+      .put('/api/v2/centers/2000000000000')
+      .send(newCenterDB)
+      .then(() => {
+      })
+      .catch((err) => {
+        err.should.have.status(404);
+      });
+  });
+});
