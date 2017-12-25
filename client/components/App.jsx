68a73c4668b4ba10756802284068a1e3dfd1acbc
Okereke Onyekachi <robocopkaka@gmail.com>
2017-12-26 00:40:26 +0100

    Add sample react code Add code to display hello world Edit client/.eslintrc to extend airbnb and not airbnb-base Add react and jsx-a11y plugins to client/.eslintrc

---
+++ client/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class App extends React.component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
