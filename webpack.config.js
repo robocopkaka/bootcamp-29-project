d05d9fdcae1629b8344754f5c580beecfc05779c
Okereke Onyekachi <robocopkaka@gmail.com>
2017-12-25 21:19:53 +0100

    Install webpack Install webpack and webpack-dev-server Create a config file for webpack in folder root

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+import path from 'path';
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  }
+}
