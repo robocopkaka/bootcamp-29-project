bfc63b9a04e12cfc225dde4c98155b76b533ffc8
Okereke Onyekachi <robocopkaka@gmail.com>
2017-12-25 21:58:11 +0100

    Add start script for webpack-dev-server Create a new script for starting webpack-dev-server Edit webpack config and use require to bring in modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-import path from 'path';
+const path = require('path');
 
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   template: './client/index.html',
   filename: 'index.html',
