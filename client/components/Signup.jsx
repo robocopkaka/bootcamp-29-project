d08b78f550dbe0d6ebde28995b560f308db1b5dc
Okereke Onyekachi <robocopkaka@gmail.com>
2018-01-03 23:23:26 +0100

    Change format of Signup component from stateless back to class

--- client/components/Signup.jsx
+++ client/components/Signup.jsx
@@ -1,50 +1,57 @@
 import React from 'react';
 
-const Signup = () => (
-  <div className="container">
-    <div className="row signup-form">
-      <div className="col s12 m12">
-        <div className="card">
-          <div className="card-content">
-            <span className="card-title"><h3 className="center-heading">Sign Up</h3></span>
-            <form className="container">
-              <div className="row">
-                <div className="input-field col s6">
-                  <input id="first_name" type="text" className="validate" />
-                  <label for="first_name">First Name</label>
-                </div>
-                <div className="input-field col s6">
-                  <input id="last_name" type="text" className="validate" />
-                  <label for="last_name">Last Name</label>
-                </div>
+class Signup extends React.Component {
+  register() {
+    const self = this;
+    alert('Account created');
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="row signup-form">
+          <div className="col s12 m12">
+            <div className="card">
+              <div className="card-content">
+                <span className="card-title"><h3 className="center-heading">Sign Up</h3></span>
+                <form className="container">
+                  <div className="row">
+                    <div className="input-field col s6">
+                      <input id="first_name" type="text" className="validate" />
+                      <label for="first_name">First Name</label>
+                    </div>
+                    <div className="input-field col s6">
+                      <input id="last_name" type="text" className="validate" />
+                      <label for="last_name">Last Name</label>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="input-field col s12">
+                      <input id="email" type="email" className="validate" />
+                      <label for="email">Email</label>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="input-field col s6">
+                      <input id="password" type="password" className="validate" />
+                      <label for="password">Password</label>
+                    </div>
+                    <div className="input-field col s6">
+                      <input id="password_confirmation" type="password" className="validate" />
+                      <label for="password_confirmation">Password Confirmation</label>
+                    </div>
+                  </div>
+                  <div className="row center-align">
+                    <button className="btn waves-effect waves-light navbar-purple round-btn" onClick={this.register} type="submit" name="action">Signup
+                      <i className="material-icons right">send</i>
+                    </button>
+                  </div>
+                </form>
               </div>
-              <div className="row">
-                <div className="input-field col s12">
-                  <input id="email" type="email" className="validate" />
-                  <label for="email">Email</label>
-                </div>
-              </div>
-              <div className="row">
-                <div className="input-field col s6">
-                  <input id="password" type="password" className="validate" />
-                  <label for="password">Password</label>
-                </div>
-                <div className="input-field col s6">
-                  <input id="password_confirmation" type="password" className="validate" />
-                  <label for="password_confirmation">Password Confirmation</label>
-                </div>
-              </div>
-              <div className="row center-align">
-                <button className="btn waves-effect waves-light navbar-purple round-btn" type="submit" name="action">Signup
-                  <i className="material-icons right">send</i>
-                </button>
-              </div>
-            </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-);
-
+    )
+  }
+}
 export default Signup;
