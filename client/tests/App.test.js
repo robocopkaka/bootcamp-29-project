03a21d075df848baced587433ae9c04cdb0eb890
Okereke Onyekachi <robocopkaka@gmail.com>
2018-01-17 19:52:32 +0100

    Install enzyme-adapter-react-16 Configure an adapter in the tests file for the App component

--- client/tests/App.test.js
+++ client/tests/App.test.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, configure } from 'enzyme';
 import { expect } from 'chai';
+import Adapter from 'enzyme-adapter-react-16';
 import App from '../components/App.jsx';
 import styles from '../components/styles.module.css';
+
+configure({ adapter: new Adapter() });
 
 describe('<App />', () => {
   let wrapper;
