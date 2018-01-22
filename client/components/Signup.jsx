7db202c63158a203ed2245028e35b99cc02757de
Okereke Onyekachi <robocopkaka@gmail.com>
2018-01-22 21:09:22 +0100

    Make the Signup component a controlled component Write functions that keep track of the data a user enters for each form element Bind all events that handle user supplied data in the component constructor

--- client/components/Signup.jsx
+++ client/components/Signup.jsx
@@ -1,6 +1,36 @@
 import React from 'react';
 
 class Signup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleFirstNameChange = this.handleFirstNameChange.bind(this);
+    this.handleLastNameChange = this.handleLastNameChange.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handlePasswordConfirmationChange = this.handlePasswordConfirmationChange.bind(this);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      passwordConfirmation: ''
+    };
+  }
+  handleFirstNameChange(e) {
+    this.setState({ firstName: e.target.value });
+  }
+  handleLastNameChange(e) {
+    this.setState({ lastName: e.target.value });
+  }
+  handleEmailChange(e) {
+    this.setState({ email: e.target.value });
+  }
+  handlePasswordChange(e) {
+    this.setState({ password: e.target.value });
+  }
+  handlePasswordConfirmationChange(e) {
+    this.setState({ passwordConfirmation: e.target.value });
+  }
   register() {
     const self = this;
     alert('Account created');
@@ -16,27 +46,57 @@ class Signup extends React.Component {
                 <form className="container">
                   <div className="row">
                     <div className="input-field col s6">
-                      <input id="first_name" type="text" className="validate" />
+                      <input
+                        id="first_name"
+                        value={this.state.firstName}
+                        type="text"
+                        className="validate"
+                        onChange={this.handleFirstNameChange}
+                      />
                       <label for="first_name">First Name</label>
                     </div>
                     <div className="input-field col s6">
-                      <input id="last_name" type="text" className="validate" />
+                      <input
+                        id="last_name"
+                        value={this.state.lastName}
+                        type="text"
+                        className="validate"
+                        onChange={this.handleLastNameChange}
+                      />
                       <label for="last_name">Last Name</label>
                     </div>
                   </div>
                   <div className="row">
                     <div className="input-field col s12">
-                      <input id="email" type="email" className="validate" />
+                      <input
+                        id="email"
+                        value={this.state.email}
+                        type="email"
+                        className="validate"
+                        onChange={this.handleEmailChange}
+                      />
                       <label for="email">Email</label>
                     </div>
                   </div>
                   <div className="row">
                     <div className="input-field col s6">
-                      <input id="password" type="password" className="validate" />
+                      <input
+                        id="password"
+                        value={this.state.password}
+                        type="password"
+                        className="validate"
+                        onChange={this.handlePasswordChange}
+                      />
                       <label for="password">Password</label>
                     </div>
                     <div className="input-field col s6">
-                      <input id="password_confirmation" type="password" className="validate" />
+                      <input
+                        id="password_confirmation"
+                        value={this.state.passwordConfirmation}
+                        type="password"
+                        className="validate"
+                        onChange={this.handlePasswordConfirmationChange}
+                      />
                       <label for="password_confirmation">Password Confirmation</label>
                     </div>
                   </div>
