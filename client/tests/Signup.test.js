81e44309da5e39bd78243e082f02eacc73f5c3e3
Okereke Onyekachi <robocopkaka@gmail.com>
2018-01-25 00:36:43 +0100

    Add tests for the Signup component

---
+++ client/tests/Signup.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { shallow, configure } from 'enzyme';
+import { expect } from 'chai';
+import Adapter from 'enzyme-adapter-react-16';
+import Signup from '../components/Signup.jsx';
+
+configure({ adapter: new Adapter() });
+
+describe('<Signup />', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<Signup />);
+  });
+  it('should have five input elements', () => {
+    expect(wrapper.find('input')).to.have.length(5);
+  });
+  it('should have a button', () => {
+    expect(wrapper.find('button')).to.have.length(1);
+  });
+});
