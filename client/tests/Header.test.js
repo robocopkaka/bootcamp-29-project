817630667d5777e9621ad25715014ca4d913c108
Okereke Onyekachi <robocopkaka@gmail.com>
2018-01-29 18:38:30 +0100

    Add more test for the Header component

--- client/tests/Header.test.js
+++ client/tests/Header.test.js
@@ -14,7 +14,21 @@ describe('<Header />', () => {
   it('should have a nav element', () => {
     expect(wrapper.find('nav').length).to.equal(1);
   });
-  // it('should have two unordered list in the nav element', () => {
-  //   expect(wrapper.find('nav').first.find('ul').length).to.equal(2);
-  // });
+  it('should have an unordered list in the nav element with an ID - nav-mobile', () => {
+    expect(wrapper.find('nav').children().find('#nav-mobile').length).to.equal(1);
+  });
+  it('should have four list elements in the unordered list with ID nav-mobile', () => {
+    expect(wrapper.find('nav').children().find('#nav-mobile').children().length).to.equal(4);
+  });
+  it('should have an unordered list in the nav element with a class - side-nav', () => {
+    expect(wrapper.find('nav').children().find('.side-nav').length).to.equal(1);
+  });
+  it('should have four list elements in the unordered list with class side-nav', () => {
+    expect(wrapper.find('nav').children().find('.side-nav').children().length).to.equal(4);
+  });
+  it('should have an unordered list inside a list element in the unordered list with a .side-nav class', () => {
+    expect(wrapper.find('nav').children().find('.side-nav').children()
+      .find('.left-padding')
+      .children().length).to.equal(1);
+  });
 });
