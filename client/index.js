ee3dae4fc544931904fe4783062a9b1c6a36ed88
Okereke Onyekachi <robocopkaka@gmail.com>
2018-01-29 21:09:46 +0100

    Render footer component on the index page

--- client/index.js
+++ client/index.js
@@ -4,8 +4,10 @@ import App from './components/App';
 import Signup from './components/Signup';
 import Login from './components/Login';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 // ReactDOM.render(<Signup />, document.getElementById('root'));
 ReactDOM.render(<Header />, document.getElementById('header'));
 ReactDOM.render(<Login />, document.getElementById('root'));
+ReactDOM.render(<Footer />, document.getElementById('footer'));
 // ReactDOM.render(<App />, document.getElementById('signup'));
