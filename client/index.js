05223406e1bbc567955a6eb1822845d95c723356
Okereke Onyekachi <robocopkaka@gmail.com>
2018-01-29 18:50:07 +0100

    Render the Header component on the index page

--- client/index.js
+++ client/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import Signup from './components/Signup';
 import Login from './components/Login';
+import Header from './components/Header';
 
 // ReactDOM.render(<Signup />, document.getElementById('root'));
+ReactDOM.render(<Header />, document.getElementById('header'));
 ReactDOM.render(<Login />, document.getElementById('root'));
 // ReactDOM.render(<App />, document.getElementById('signup'));
