020c20420cf964c3fb604623438f4a33e5fd9494
Okereke Onyekachi <robocopkaka@gmail.com>
2018-01-31 01:59:31 +0100

    Render Home component to root directory

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -5,21 +5,21 @@ const Home = () => {
     <div className="slider">
       <ul className="slides">
         <li>
-          <img src="img/events.jpeg" alt="" />
+          <img src="../../template/img/events.jpeg" alt="" />
           <div className="caption center-align">
-            <h3 className="color-black">Kachi's Event Manager</h3>
+            <h3 className="color-black">Kachi&#39;s Event Manager</h3>
           </div>
         </li>
         <li>
-          <img src="img/events.jpeg" alt="" />
+          <img src="../../template/img/events.jpeg" alt="" />
           <div className="caption left-align">
             <h3 className="color-black">Find centers that suit you</h3>
           </div>
         </li>
         <li>
-          <img src="img/events.jpeg" alt="" />
+          <img src="../../template/img/events.jpeg" alt="" />
           <div className="caption right-align">
-            <h3 className="color-black">And events you'll want to attend</h3>
+            <h3 className="color-black">And events you&#39;ll want to attend</h3>
           </div>
         </li>
       </ul>
