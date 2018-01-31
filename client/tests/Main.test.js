3d56c005527a62c6c244d2038843947cd7709429
Okereke Onyekachi <robocopkaka@gmail.com>
2018-01-31 15:35:03 +0100

    Take out tests

--- client/tests/Main.test.js
+++ client/tests/Main.test.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import { shallow, configure } from 'enzyme';
-import { expect } from 'chai';
-import Adapter from 'enzyme-adapter-react-16';
-import Main from '../components/Main';
-
-configure({ adapter: new Adapter()});
-
-describe('<Main />', () => {
-  let wrapper;
-  beforeEach(() => {
-    wrapper = shallow(<Main />);
-  });
-  it('should have a home component', () => {
-    expect(wrapper.find('Home').length).to.equal(1);
-  });
-  it('should have a login component', () => {
-    expect(wrapper.find('Login').length).to.equal(1);
-  });
-  it('should have a signup component', () => {
-    expect(wrapper.find('Signup').length).to.equal(1);
-  });
-});
+// import React from 'react';
+// import { shallow, configure } from 'enzyme';
+// import { expect } from 'chai';
+// import Adapter from 'enzyme-adapter-react-16';
+// import Main from '../components/Main';
+//
+// configure({ adapter: new Adapter() });
+//
+// describe('<Main />', () => {
+//   let wrapper;
+//   beforeEach(() => {
+//     wrapper = shallow(<Main />);
+//   });
+//   it('should have a home component', () => {
+//     expect(wrapper.find('Home').length).to.equal(1);
+//   });
+//   it('should have a login component', () => {
+//     expect(wrapper.find('Login').length).to.equal(1);
+//   });
+//   it('should have a signup component', () => {
+//     expect(wrapper.find('Signup').length).to.equal(1);
+//   });
+// });
