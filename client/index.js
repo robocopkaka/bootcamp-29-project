920afabc1caafb1328f323a572694acc2de66f6a
Okereke Onyekachi <robocopkaka@gmail.com>
2018-02-01 23:27:24 +0100

    Create index.js file in the reducers folder

--- client/index.js
+++ client/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import reducers from './reducers';
 import App from './components/App';
 // import Signup from './components/Signup';
 // import Login from './components/Login';
