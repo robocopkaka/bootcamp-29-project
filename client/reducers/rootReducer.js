a42e74544cf3f2e69a7796e73eaf72696033b566
Okereke Onyekachi <robocopkaka@gmail.com>
2018-02-06 00:36:46 +0100

    Add the registerReducer to the rootReducer object

--- client/reducers/rootReducer.js
+++ client/reducers/rootReducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import session from './sessionReducer';
+import register from './registerReducer';
 
 const rootReducer = combineReducers({
-  session
+  session,
+  register
 });
 export default rootReducer;
