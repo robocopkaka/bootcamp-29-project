97850df94b8d1221d079fe710cb77c32321ceae4
Okereke Onyekachi <robocopkaka@gmail.com>
2018-02-05 06:53:24 +0100

    Create a class that makes a call to the login API

---
+++ client/api/sessionApi.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+class SessionApi {
+  static login(credentials) {
+    axios.post(
+      'http://localhost:8000/api/v2/users/login',
+      JSON.stringify({
+        email: credentials.email,
+        password: credentials.password
+      }),
+      { headers: { 'Content-Type': 'application/json' } }
+    )
+      .then((res) => {
+        return res;
+      })
+      .catch((err) => {
+        return err;
+      });
+  }
+}
+export default SessionApi;
