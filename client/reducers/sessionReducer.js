4d1b8ff3346a58eb8b04f3f2c4387a53cc3a8efa
Okereke Onyekachi <robocopkaka@gmail.com>
2018-02-05 07:27:31 +0100

    Create a session reducer

---
+++ client/reducers/sessionReducer.js
@@ -0,0 +1,14 @@
+import initialState from './initialState';
+
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+
+export default function sessionReducer(state = initialState.session, action) {
+  switch (action.type) {
+    case LOGIN_SUCCESS:
+      history.push('/');
+      return !!sessionStorage.jwt;
+      break;
+    default:
+      return state;
+  }
+}
