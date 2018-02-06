fdcccf1f80744785f6bd6b1d1f9dfe735c23a69d
Okereke Onyekachi <robocopkaka@gmail.com>
2018-02-07 00:13:25 +0100

    Rewrite Home component as a class component Use componentDidMount to load the slider from materialize

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,30 +1,35 @@
 import React from 'react';
 import events from '../img/events.jpeg';
 
-const Home = () => {
-  return (
-    <div className="slider">
-      <ul className="slides">
-        <li>
-          <img src={events} alt="" />
-          <div className="caption center-align">
-            <h3 className="color-black">Kachi&#39;s Event Manager</h3>
-          </div>
-        </li>
-        <li>
-          <img src={events} alt="" />
-          <div className="caption left-align">
-            <h3 className="color-black">Find centers that suit you</h3>
-          </div>
-        </li>
-        <li>
-          <img src={events} alt="" />
-          <div className="caption right-align">
-            <h3 className="color-black">And events you&#39;ll want to attend</h3>
-          </div>
-        </li>
-      </ul>
-    </div>
-  );
-};
+class Home extends React.Component {
+  componentDidMount() {
+    $('.slider').slider();
+  }
+  render() {
+    return (
+      <div className="slider">
+        <ul className="slides">
+          <li>
+            <img src={events} alt="" />
+            <div className="caption center-align">
+              <h3 className="color-black">Kachi&#39;s Event Manager</h3>
+            </div>
+          </li>
+          <li>
+            <img src={events} alt="" />
+            <div className="caption left-align">
+              <h3 className="color-black">Find centers that suit you</h3>
+            </div>
+          </li>
+          <li>
+            <img src={events} alt="" />
+            <div className="caption right-align">
+              <h3 className="color-black">And events you&#39;ll want to attend</h3>
+            </div>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
 export default Home;
