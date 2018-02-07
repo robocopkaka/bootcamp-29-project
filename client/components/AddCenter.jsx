189739e4e6a27184e32f628b50a1ce30cd16f671
Okereke Onyekachi <robocopkaka@gmail.com>
2018-02-07 02:14:13 +0100

    Add methods for checking if a form is valid and resetting validation states

--- client/components/AddCenter.jsx
+++ client/components/AddCenter.jsx
@@ -28,5 +28,52 @@ class AddCenter extends Component {
     this.setState({ state: { value: '', isValid: true, message: '' } });
     this.setState({ detail: { value: '', isValid: true, message: '' } });
   }
-  
+  formIsValid() {
+    let fieldCheck = true;
+    const state = Object.assign({}, this.state);
+
+    if (validator.isEmpty(state.name.value)) {
+      state.name.isValid = false;
+      state.name.message = 'Name must not be empty';
+
+      this.setState({ name: state.name });
+      fieldCheck = false;
+    }
+    if (validator.isEmpty(state.address.value)) {
+      state.address.isValid = false;
+      state.address.message = 'Address must not be empty';
+
+      this.setState({ email: state.address });
+      fieldCheck = false;
+    }
+    if (validator.isEmpty(state.state.value)) {
+      state.state.isValid = false;
+      state.state.message = 'State must not be empty';
+
+      this.setState({ state: state.state });
+      fieldCheck = false;
+    }
+    if (validator.isEmpty(state.detail.value)) {
+      state.detail.isValid = false;
+      state.detail.message = 'Email must not be empty';
+
+      this.setState({ email: state.detail });
+      fieldCheck = false;
+    }
+    if (!fieldCheck) {
+      return false;
+    }
+    return true;
+  }
+  resetValidationStates() {
+    const state = Object.assign({}, this.state);
+
+    Object.keys(state).map((key) => {
+      if ({}.hasOwnProperty.call(state[key], 'isValid')) {
+        state[key].isValid = true;
+        state[key].message = '';
+      }
+    });
+    this.setState(state);
+  }
 }
