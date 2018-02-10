5b38780dcf8b8af78e40d6f9acc7619fde6c5802
Okereke Onyekachi <robocopkaka@gmail.com>
2018-02-10 02:37:13 +0100

    Create CenterList component

---
+++ client/components/CenterList.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CenterList = ({ centers }) => {
+  return (
+    <div>
+      {centers.map(center => (
+        <div className="card" key={center.id}>
+          <div className="card-image">
+            <a href="show-center.html"><img src={center.image} alt="" /></a>
+          </div>
+          <div className="card-content">
+            <span className="center-name">
+              <a href="show-center.html">{center.name}</a>
+            </span><br />
+            <span className="center-state">
+              <a href="show-center.html">{center.state}</a>
+            </span><br />
+            <span className="center-address">{center.address}</span>
+          </div>
+          <div className="card-action">
+            <a
+              className="waves-effect waves-light btn navbar-purple round-btn white-color"
+              href="show-center.html"
+            >
+              View
+            </a>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+CenterList.propTypes = {
+  centers: PropTypes.arrayOf(PropTypes.object).isRequired
+};
+export default CenterList;
--- client/components/Centers.jsx
+++ client/components/Centers.jsx
@@ -16,11 +16,17 @@ class Centers extends Component {
   render() {
     return (
       <div className="container">
+        <Search />
         <div class="top-ten-padding"></div>
         <div className="row">
-          <div class="col s12 m6 l4">
+          <div  className="col s12 m6 l4">
             <CenterList centers={this.props.centers} />
           </div>
+        </div>
+        <div className="fixed-action-btn horizontal click-to-toggle">
+          <a className="btn-floating btn-large red white-color" href="new-center.html">
+            <i className="material-icons">add</i>
+          </a>
         </div>
       </div>
     );
