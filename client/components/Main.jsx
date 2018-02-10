2a7e99a8eca26ff38b95812abbc748c43e6afb52
Okereke Onyekachi <robocopkaka@gmail.com>
2018-02-10 03:12:37 +0100

    Add route for the Centers component Add link to the Centers component to the Header component

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -4,12 +4,14 @@ import Home from './Home';
 import Login from './Login';
 import Signup from './Signup';
 import AddCenter from './AddCenter';
+import Centers from './Centers';
 
 const Main = () => (
   <Switch>
     <Route exact path="/" component={Home} />
     <Route path="/login" component={Login} />
     <Route path="/signup" component={Signup} />
+    <Route path="/centers" component={Centers} />
     <Route path="/add-center" component={AddCenter} />
   </Switch>
 );
