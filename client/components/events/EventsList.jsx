6ca93bb77b221dd593abcda21edfd4e874365044
Okereke Onyekachi <robocopkaka@gmail.com>
2018-02-13 01:22:04 +0100

    Use classnames to group classes from events.module.css and template/css/index.css

--- client/components/events/EventsList.jsx
+++ client/components/events/EventsList.jsx
@@ -1,39 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import * as styles from '../../css/events.module.css';
 
-const EventsList = ({ events }) => (
-  <div>
-    { /*
-      <div className="show-center-top"><span className="date">Thursday, November 22</span></div>
-    */ }
-    <div className="row">
-      {events.map(event => (
-        <div className="col s12 m12 l12">
-          <div className="card-panel horizontal">
-            <div className="row event-row">
-              <div className={styles['fifteen-percent']}>
-                <div className="col s3 m3 l3">
+const EventsList = ({ events }) => {
+  const eightFivePercentClasses = classNames('col', 's9', 'm9', 'l9', styles['eighty-five-percent']);
+  const fifteenPercentClasses = classNames(styles['ifteen-percent'], 'col', 's3', 'm3', 'l3');
+  return (
+    <div>
+      { /*
+        <div className="show-center-top"><span className="date">Thursday, November 22</span></div>
+      */ }
+      <div className="row">
+        {events.map(event => (
+          <div className="col s12 m12 l12">
+            <div className="card-panel horizontal">
+              <div className="row">
+                <div className={fifteenPercentClasses}>
                   <span>{event.date}</span>
                 </div>
-              </div>
-              <div className={styles['eighty-five-percent']}>
-                <div className="col s9 m9 l9">
+                <div className={eightFivePercentClasses}>
                   <span className={styles['event-focus']}>
                     <a href="show-event.html">{event.name}</a>
                   </span><br />
-                <span className={styles['event-title']}>
+                  <span className="event-title">
                     <a href="show-event.html">{event.detail}</a>
                   </span>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+}
 EventsList.propTypes = {
   events: PropTypes.objectOf.isRequired
 };
