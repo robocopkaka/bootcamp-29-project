e0bed76011076ce643d1bda5e11a5188eadce19d
Okereke Onyekachi <robocopkaka@gmail.com>
2018-02-18 04:51:54 +0100

    Create action and action creator for updating centers

--- client/actions/centerActions.js
+++ client/actions/centerActions.js
@@ -7,10 +7,9 @@ export function addCenterSuccess(center) {
 export function fetchCentersSuccess(centers) {
   return { type: types.FETCH_CENTERS_SUCCESS, centers };
 }
-// export function fetchSingleCenterSuccess(center) {
-//   return { type: types.FETCH_SINGLE_CENTER_SUCCESS, center };
-// }
-
+export function updateCenterSuccess(center) {
+  return { type: types.UPDATE_CENTER_SUCCESS, center };
+}
 export function addCenter(values) {
   return function (dispatch) {
     return CenterApi.create(values)
@@ -33,14 +32,12 @@ export function fetchCenters() {
       });
   };
 }
-// export function fetchSingleCenter(centerId) {
-//   return function (dispatch) {
-//     return CenterApi.getOne(centerId)
-//       .then((response) => {
-//         dispatch(fetchSingleCenterSuccess(response.data.center));
-//       })
-//       .catch((error) => {
-//         console.log(error);
-//       });
-//   }
-// }
+export function updateCenter(center) {
+  return function (dispatch) {
+    return CenterApi.update(center)
+      .then((response) => {
+        dispatch(updateCenterSuccess(response.date.center))
+      })
+      .catch((error) => console.log(error));
+  };
+}
