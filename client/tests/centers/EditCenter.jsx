8d72432c941505e146771c64d972427e51ce980f
Okereke Onyekachi <robocopkaka@gmail.com>
2018-02-18 03:24:16 +0100

    Add HTML code for editing centers to the EditCenter component

--- client/tests/centers/EditCenter.jsx
+++ client/tests/centers/EditCenter.jsx
@@ -7,7 +7,66 @@ import validator from 'validator';
 import * as singleCenterActions from '../../actions/singleCenterActions';
 
 class EditCenter extends Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="container max-width-six-hundred">
+        <div className="card">
+          <div className="container">
+            <h3 className="center-heading">Edit a Center</h3>
+          </div>
+          <form className="card-content">
+            <div className="row">
+              <div className="input-field col s12">
+                <input id="center-name" type="text" className="validate" value="Kachi's Center" />
+                <label htmlFor="center-name">Name</label>
+              </div>
+            </div>
+            <div className="row">
+              <div className="input-field col s12">
+                <input id="center-address" type="text" className="validate" value="Gowon Estate" />
+                <label htmlFor="center-address">Address</label>
+              </div>
+            </div>
+            <div className="row">
+              <div className="input-field col s12">
+                <input id="center-state" type="text" className="validate" value="Lagos" />
+                <label htmlFor="center-state">State</label>
+              </div>
+            </div>
+            <div className="row">
+              <div className="input-field col s12">
+                <div className="chips chips-initial"></div>
+              </div>
+            </div>
+            <div clasNames="row">
+              <div className="file-field input-field">
+                <div className="btn navbar-purple round-btn">
+                  <span>Image</span>
+                  <input type="file" />
+                </div>
+                <div className="file-path-wrapper">
+                  <input className="file-path validate" type="text" value="owen shaw.jpg" />
+                </div>
+              </div>
+            </div>
+            <div className="row center-align">
+              <button
+                className="btn waves-effect waves-light navbar-purple round-btn"
+                type="submit"
+                name="action"
+              >Update Center
+                <i className="material-icons right">send</i>
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 EditCenter.propTypes = {
 
