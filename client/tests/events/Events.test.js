95c18fc66f31c96679e3aa8fef954f7e5f9d0573
Okereke Onyekachi <robocopkaka@gmail.com>
2018-02-20 03:24:43 +0100

    Write tests for the Events component

---
+++ client/tests/events/Events.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { mount, configure } from 'enzyme';
+import { expect } from 'chai';
+import Adapter from 'enzyme-adapter-react-16';
+import Events from '../../components/events/Events';
+import EventsListWithImage from '../../components/events/EventsListWithImage';
+import Search from '../../components/Search';
+
+configure({ adapter: new Adapter() });
+
+describe('<Events />', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = mount(<Events />);
+  });
+  it('should have a parent div with a .container class', () => {
+    expect(wrapper.find('.container').length).to.equal();
+  });
+  it('should render a Search component', () => {
+    expect(wrapper.find(Search).length).to.equal(1);
+  });
+  it('should have an EventsListWithImage component', () => {
+    expect(wrapper.find(EventsListWithImage).length).to.equal(1);
+  });
+});
