38e43fb21d8cd32791da573c695a42e423755dbb
Okereke Onyekachi <robocopkaka@gmail.com>
2018-02-21 03:53:43 +0100

    Create AddEvent component Add more tests for the AddEvent component

---
+++ client/components/events/AddEvent.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import moment from 'moment';
+import * as eventActions from '../../actions/eventActions';
+
+class AddEvent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: { value: '', isValid: true, message: '' },
+      detail: { value: '', isValid: true, message: '' },
+      guests: { value: '', isValid: true, message: '' },
+      date: { value: '', isValid: true, message: '' },
+      time: { value: '', isValid: true, message: '' }
+    };
+  }
+  componentDidMount() {
+    $('.datepicker').pickadate({
+      selectMonths: true, // Creates a dropdown to control month
+      selectYears: 15, // Creates a dropdown of 15 years to control year,
+      today: 'Today',
+      clear: 'Clear',
+      close: 'Ok',
+      closeOnSelect: false // Close upon selecting a date,
+    });
+    $('.timepicker').pickatime({
+      default: 'now', // Set default time: 'now', '1:30AM', '16:30'
+      fromnow: 0, // set default time to * milliseconds from now (using with default = 'now')
+      twelvehour: false, // Use AM/PM or 24-hour format
+      donetext: 'OK', // text for done-button
+      cleartext: 'Clear', // text for clear-button
+      canceltext: 'Cancel', // Text for cancel-button
+      autoclose: false, // automatic close timepicker
+      ampmclickable: true, // make AM PM clickable
+      aftershow: function (){} //Function for after opening timepicker
+    });
+  }
+}
+AddEvent.propTypes = {
+  actions: PropTypes.objectOf(PropTypes.func).isRequired
+};
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(eventActions, dispatch)
+  };
+}
+export default connect(null, mapDispatchToProps)(AddEvent)
--- client/tests/events/AddEvent.test.js
+++ client/tests/events/AddEvent.test.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { shallow, configure } from 'enzyme';
+import { mount, configure } from 'enzyme';
 import { expect } from 'chai';
 import Adapter from 'enzyme-adapter-react-16';
 import AddEvent from '../../components/events/AddEvent';
+import SelectCenter from '../../centers/SelectCenter';
 
 configure({ adapter: new Adapter() });
 
 describe('<AddEvent />', () => {
   let wrapper;
   beforeEach(() => {
-    wrapper = shallow(<AddEvent />);
+    wrapper = mount(<AddEvent />);
   });
   it('should have two divs with a .container class', () => {
     expect(wrapper.find('.container').length).to.equal(2);
@@ -18,6 +19,12 @@ describe('<AddEvent />', () => {
     expect(wrapper.find('.card').length).to.equal(1);
   });
   it('should have a button', () => {
-    expect(wrapper.find('.button').length).to.equal(1);
+    expect(wrapper.find('button').length).to.equal(1);
+  });
+  it('should have a form', () => {
+    expect(wrapper.find('form').length).to.equal(1);
+  });
+  it('should render a SelectCenter component', () => {
+    expect(wrapper.find(SelectCenter).length).to.equal(1);
   });
 });
