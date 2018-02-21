37a1fb3803bf13fd41b04d774f9962520ae94aea
Okereke Onyekachi <robocopkaka@gmail.com>
2018-02-21 02:05:38 +0100

    Create static method for retrieving a single event

--- client/api/eventApi.js
+++ client/api/eventApi.js
@@ -18,5 +18,22 @@ class EventApi {
         return error;
       })
   }
+  static getOne(eventId) {
+    return axios.get(
+      `http://localhost:8000/api/v2/events/${eventId}`,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      }
+    )
+      .then((response) => {
+        return response;
+      })
+      .catch((error) => {
+        console.log('CATCH = ', error.response);
+        return error;
+      });
+  }
 }
 export default EventApi;
