2a4acc058d5e9617abee122606d4f1b67d67dffb
Okereke Onyekachi <robocopkaka@gmail.com>
2018-02-21 02:02:35 +0100

    Create action and action creator for fetching a single event

---
+++ client/actions/singleEventActions.js
@@ -0,0 +1,18 @@
+import * as types from './actionTypes';
+import EventApi from '../api/eventApi';
+
+export function fetchSingleEventSuccess(event) {
+  return { type: types.FETCH_SINGLE_EVENT_SUCCESS, event };
+}
+
+export function fetchSingleEvent(eventId) {
+  return function (dispatch) {
+    return EventApi.getOne(id)
+      .then((response) => {
+        dispatch(fetchSingleEventSuccess(response.data.event));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+}
