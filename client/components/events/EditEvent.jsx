35ecd5272a4e0d9cd38792cc9f8624ba41495432
Okereke Onyekachi <robocopkaka@gmail.com>
2018-02-23 02:21:21 +0100

    Add a componentDidMount method and a componentWillReceiveProps method

--- client/components/events/EditEvent.jsx
+++ client/components/events/EditEvent.jsx
@@ -21,138 +21,155 @@ class EditEvent extends Component {
       centers: {}
     };
   }
+  componentDidMount() {
+    this.props.actions.fetchSingleEvent(parseInt(this.props.match.params.id, 10));
+  }
+  componentWillReceiveProps(nextProps) {
+    if (this.props.event.id !== nextProps.event.id) {
+      this.setState({
+        name: Object.assign({}, this.state.name, { value: nextProps.event.name }),
+        detail: Object.assign({}, this.state.detail, { value: nextProps.event.detail }),
+        guests: Object.assign({}, this.state.guests, { value: nextProps.event.guests }),
+        center: Object.assign({}, this.state.center, {
+          value:( nextProps.event.centerId).toString()
+        }),
+        category: Object.assign({}, this.state.category, {
+          value: (nextProps.event.categoryId).toString()
+        })
+      })
+    }
+  }
   render() {
+    const { centers = [] } = this.props;
+    const nameClasses = classNames('help-block', { 'has-error': !this.state.name.isValid });
+    const detailClasses = classNames('help-block', { 'has-error': !this.state.detail.isValid });
+    const guestsClasses = classNames('help-block', { 'has-error': !this.state.guests.isValid });
+    const dateClasses = classNames('help-block', { 'has-error': !this.state.date.isValid });
+    const timeClasses = classNames('help-block', { 'has-error': !this.state.time.isValid });
+    const centerClasses = classNames('help-block', { 'has-error': !this.state.center.isValid });
+    const categoryClasses = classNames('help-block', { 'has-error': !this.state.category.isValid });
+    const containerClasses = classNames('container max-width-six-hundred');
     return (
-      const { centers = [] } = this.props;
-      const nameClasses = classNames('help-block', { 'has-error': !this.state.name.isValid });
-      const detailClasses = classNames('help-block', { 'has-error': !this.state.detail.isValid });
-      const guestsClasses = classNames('help-block', { 'has-error': !this.state.guests.isValid });
-      const dateClasses = classNames('help-block', { 'has-error': !this.state.date.isValid });
-      const timeClasses = classNames('help-block', { 'has-error': !this.state.time.isValid });
-      const centerClasses = classNames('help-block', { 'has-error': !this.state.center.isValid });
-      const categoryClasses = classNames('help-block', { 'has-error': !this.state.category.isValid });
-      const containerClasses = classNames('container max-width-six-hundred');
-      return (
-        <div className={containerClasses}>
-          <div className="card">
-            <div className="container">
-              <h3 className="center-heading">Add an Event</h3>
-            </div>
-            <form className="card-content">
-              <div className="row">
-                <div className="input-field col s12">
-                  <input
-                    id="event-name"
-                    name="name"
-                    value={this.state.name.value}
-                    type="text"
-                    className="validate"
-                    onChange={this.handleChange}
-                  />
-                  <label for="event-name">Name</label>
-                  <span className={nameClasses}>{this.state.name.message}</span>
-                </div>
-              </div>
-              <div className="row">
-                <div className="input-field col s12">
-                  <textarea
-                    id="event-detail"
-                    name="detail"
-                    value={this.state.detail.value}
-                    className="materialize-textarea validate"
-                    onChange={this.handleChange}
-                  ></textarea>
-                  <label for="center-detail">Detail</label>
-                  <span className={detailClasses}>{this.state.detail.message}</span>
-                </div>
-              </div>
-              <div className="row">
-                <div className="input-field col s6">
-                  <input
-                    id="event-guests"
-                    name="guests"
-                    value={this.state.guests.value}
-                    type="number"
-                    className="validate"
-                    onChange={this.handleChange}
-                  />
-                  <label for="event-guests">Guests</label>
-                  <span className={guestsClasses}>{this.state.guests.message}</span>
-                </div>
-              </div>
-              <div className="row">
-                <div className="input-field col s12">
-                  <input
-                    name="date"
-                    value={this.state.date.value}
-                    type="text"
-                    className="datepicker"
-                    id="event-date"
-                  />
-                  <label for="event-date">Date</label>
-                  <span className={dateClasses}>{this.state.date.message}</span>
-                </div>
-              </div>
-              <div className="row">
-                <div className="input-field col s12">
-                  <input
-                    id="event-time"
-                    name="time"
-                    value={this.state.time.value}
-                    className="timepicker"
-                    type="text"
-                    onChange={this.handleTimeChange}
-                  />
-                  <label for="event-time">Time</label>
-                  <span className={timeClasses}>{this.state.time.message}</span>
-                </div>
-              </div>
-              <div className="row">
-                <div className="input-field col s16">
-                  <select
-                    name="center"
-                    value={this.state.center.value}
-                    onChange={this.handleSelectCenterChange}
-                    id="event-center"
-                  >
-                    <option value="">Pick a Center</option>
-                    {centers.map(center => (
-                      <option
-                        key={center.id}
-                        value={center.id}
-                      >{center.name}
-                      </option>
-                    ))}
-                  </select>
-                  <label htmlFor="event-center">Center</label>
-                  <span className={centerClasses}>{this.state.center.message}</span>
-                </div>
-                <div className="input-field col s16">
-                  <select
-                    name="category"
-                    value={this.state.category.value}
-                    onChange={this.handleSelectCategoryChange}
-                    id="event-category"
-                  >
-                    <option value="1">General</option>
-                  </select>
-                  <label htmlFor="event-category">Category</label>
-                  <span className={categoryClasses}>{this.state.category.message}</span>
-                </div>
-              </div>
-              <div className="row center-align">
-                <button
-                  className="btn waves-effect waves-light navbar-purple round-btn"
-                  type="submit"
-                  name="action"
-                  onClick={this.addEvent}
-                >Add Event
-                  <i className="material-icons right">send</i>
-                </button>
-              </div>
-            </form>
+      <div className={containerClasses}>
+        <div className="card">
+          <div className="container">
+            <h3 className="center-heading">Add an Event</h3>
           </div>
+          <form className="card-content">
+            <div className="row">
+              <div className="input-field col s12">
+                <input
+                  id="event-name"
+                  name="name"
+                  value={this.state.name.value}
+                  type="text"
+                  className="validate"
+                  onChange={this.handleChange}
+                />
+                <label for="event-name">Name</label>
+                <span className={nameClasses}>{this.state.name.message}</span>
+              </div>
+            </div>
+            <div className="row">
+              <div className="input-field col s12">
+                <textarea
+                  id="event-detail"
+                  name="detail"
+                  value={this.state.detail.value}
+                  className="materialize-textarea validate"
+                  onChange={this.handleChange}
+                ></textarea>
+                <label for="center-detail">Detail</label>
+                <span className={detailClasses}>{this.state.detail.message}</span>
+              </div>
+            </div>
+            <div className="row">
+              <div className="input-field col s6">
+                <input
+                  id="event-guests"
+                  name="guests"
+                  value={this.state.guests.value}
+                  type="number"
+                  className="validate"
+                  onChange={this.handleChange}
+                />
+                <label for="event-guests">Guests</label>
+                <span className={guestsClasses}>{this.state.guests.message}</span>
+              </div>
+            </div>
+            <div className="row">
+              <div className="input-field col s12">
+                <input
+                  name="date"
+                  value={this.state.date.value}
+                  type="text"
+                  className="datepicker"
+                  id="event-date"
+                />
+                <label for="event-date">Date</label>
+                <span className={dateClasses}>{this.state.date.message}</span>
+              </div>
+            </div>
+            <div className="row">
+              <div className="input-field col s12">
+                <input
+                  id="event-time"
+                  name="time"
+                  value={this.state.time.value}
+                  className="timepicker"
+                  type="text"
+                  onChange={this.handleTimeChange}
+                />
+                <label for="event-time">Time</label>
+                <span className={timeClasses}>{this.state.time.message}</span>
+              </div>
+            </div>
+            <div className="row">
+              <div className="input-field col s16">
+                <select
+                  name="center"
+                  value={this.state.center.value}
+                  onChange={this.handleSelectCenterChange}
+                  id="event-center"
+                >
+                  <option value="">Pick a Center</option>
+                  {centers.map(center => (
+                    <option
+                      key={center.id}
+                      value={center.id}
+                    >{center.name}
+                    </option>
+                  ))}
+                </select>
+                <label htmlFor="event-center">Center</label>
+                <span className={centerClasses}>{this.state.center.message}</span>
+              </div>
+              <div className="input-field col s16">
+                <select
+                  name="category"
+                  value={this.state.category.value}
+                  onChange={this.handleSelectCategoryChange}
+                  id="event-category"
+                >
+                  <option value="1">General</option>
+                </select>
+                <label htmlFor="event-category">Category</label>
+                <span className={categoryClasses}>{this.state.category.message}</span>
+              </div>
+            </div>
+            <div className="row center-align">
+              <button
+                className="btn waves-effect waves-light navbar-purple round-btn"
+                type="submit"
+                name="action"
+                onClick={this.addEvent}
+              >Update Event
+                <i className="material-icons right">send</i>
+              </button>
+            </div>
+          </form>
         </div>
+      </div>
     );
   }
 }
