8b780d0209436a6dfa17b90b69d29cc59062a612
Okereke Onyekachi <robocopkaka@gmail.com>
2018-02-27 05:27:20 +0100

    Remove Match from imports in Main component

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route, Redirect, Match } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Home from './Home';
 import Login from './Login';
 import Signup from './Signup';
