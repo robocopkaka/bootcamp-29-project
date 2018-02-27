49bb39802af1f3d9432bea20345018cdb9a5f204
Okereke Onyekachi <robocopkaka@gmail.com>
2018-02-27 03:29:02 +0100

    Show links for logging out if user is logged in

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,60 +1,130 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const Header = () => (
-  <div>
-    <ul id="signup-dropdown" className="dropdown-content">
-      <li><Link to="/login">Login</Link></li>
-      <li><Link to="/signup">Signup</Link></li>
-    </ul>
-    <nav className="navbar-purple">
-      <div className="nav-wrapper left-padding">
-        <Link to="/" className="brand-logo">EventManager</Link>
-        <a
-          href="index.html"
-          data-activates="side-menu"
-          className="button-collapse"
-        >
-          <i className="material-icons">menu</i>
-        </a>
-        <ul id="nav-mobile" className="right hide-on-med-and-down">
-          <li className="active"><Link to="/">Home</Link></li>
-          <li><Link to="/events">Events</Link></li>
-          <li><Link to="/centers">Centers</Link></li>
-          <li>
-            <a
-              className="dropdown-button"
-              href="#!"
-              data-activates="signup-dropdown"
-            >
-                Login<i className="material-icons right">arrow_drop_down</i>
-            </a>
-          </li>
-        </ul>
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.stuff = false;
+  }
+  render() {
+    if (this.props.logged_in) {
+      return(
+        <div>
+          <ul id="signup-dropdown" className="dropdown-content">
+            <li><Link to="/login">Logout</Link></li>
+            <li><Link to="/signup">Profile</Link></li>
+          </ul>
+          <nav className="navbar-purple">
+            <div className="nav-wrapper left-padding">
+              <Link to="/" className="brand-logo">EventManager</Link>
+              <a
+                href="index.html"
+                data-activates="side-menu"
+                className="button-collapse"
+              >
+                <i className="material-icons">menu</i>
+              </a>
+              <ul id="nav-mobile" className="right hide-on-med-and-down">
+                <li className="active"><Link to="/">Home</Link></li>
+                <li><Link to="/events">Events</Link></li>
+                <li><Link to="/centers">Centers</Link></li>
+                <li>
+                  <a
+                    className="dropdown-button"
+                    href="#!"
+                    data-activates="signup-dropdown"
+                  >
+                      Logout<i className="material-icons right">arrow_drop_down</i>
+                  </a>
+                </li>
+              </ul>
 
-        <ul className="side-nav" id="side-menu">
-          <li className="active"><Link to="/">Home</Link></li>
-          <li><Link to="/events">Events</Link></li>
-          <li><Link to="/centers">Centers</Link></li>
-          <li className="left-padding">
-            <ul className="collapsible collapsible-accordion">
-              <li>
-                <a className="collapsible-header" href="#">Login
-                  <i className="material-icons right">arrow_drop_down</i>
-                </a>
-                <div className="collapsible-body">
-                  <ul>
-                    <li><Link to="/login">Login</Link></li>
-                    <li><Link to="/signup">Signup</Link></li>
+              <ul className="side-nav" id="side-menu">
+                <li className="active"><Link to="/">Home</Link></li>
+                <li><Link to="/events">Events</Link></li>
+                <li><Link to="/centers">Centers</Link></li>
+                <li className="left-padding">
+                  <ul className="collapsible collapsible-accordion">
+                    <li>
+                      <a className="collapsible-header" href="#">Logout
+                        <i className="material-icons right">arrow_drop_down</i>
+                      </a>
+                      <div className="collapsible-body">
+                        <ul>
+                          <li><Link to="/login">Logout</Link></li>
+                          <li><Link to="/signup">Profile</Link></li>
+                        </ul>
+                      </div>
+                    </li>
                   </ul>
-                </div>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </div>
-);
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <ul id="signup-dropdown" className="dropdown-content">
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/signup">Signup</Link></li>
+          </ul>
+          <nav className="navbar-purple">
+            <div className="nav-wrapper left-padding">
+              <Link to="/" className="brand-logo">EventManager</Link>
+              <a
+                href="index.html"
+                data-activates="side-menu"
+                className="button-collapse"
+              >
+                <i className="material-icons">menu</i>
+              </a>
+              <ul id="nav-mobile" className="right hide-on-med-and-down">
+                <li className="active"><Link to="/">Home</Link></li>
+                <li><Link to="/events">Events</Link></li>
+                <li><Link to="/centers">Centers</Link></li>
+                <li>
+                  <a
+                    className="dropdown-button"
+                    href="#!"
+                    data-activates="signup-dropdown"
+                  >
+                      Login<i className="material-icons right">arrow_drop_down</i>
+                  </a>
+                </li>
+              </ul>
 
-export default Header;
+              <ul className="side-nav" id="side-menu">
+                <li className="active"><Link to="/">Home</Link></li>
+                <li><Link to="/events">Events</Link></li>
+                <li><Link to="/centers">Centers</Link></li>
+                <li className="left-padding">
+                  <ul className="collapsible collapsible-accordion">
+                    <li>
+                      <a className="collapsible-header" href="#">Login
+                        <i className="material-icons right">arrow_drop_down</i>
+                      </a>
+                      <div className="collapsible-body">
+                        <ul>
+                          <li><Link to="/login">Login</Link></li>
+                          <li><Link to="/signup">Signup</Link></li>
+                        </ul>
+                      </div>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      );
+    }
+  }
+}
+function mapStateToProps(state) {
+  return { logged_in: state.session };
+}
+
+export default connect(mapStateToProps)(Header);
