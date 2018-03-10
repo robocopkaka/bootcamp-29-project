524f241df76bdbd807825b6f0f82f2f6a40b5eb6
Okereke Onyekachi <robocopkaka@gmail.com>
2018-03-10 15:39:09 +0100

    Add action for loading when registering a user

--- client/actions/registerActions.js
+++ client/actions/registerActions.js
@@ -9,8 +9,13 @@ export function registerFailure(response) {
   return { type: types.REGISTER_FAILURE, response };
 }
 
+export function registerLoading() {
+  return { type: types.REGISTER_LOADING };
+}
+
 export function registerUser(credentials) {
   return (dispatch) => {
+    dispatch(registerLoading());
     return RegisterApi.register(credentials)
       .then((response) => {
         sessionStorage.setItem('registered', true);
