d0e96bbff0786d665ebf56a6530745a2e7323004
Okereke Onyekachi <robocopkaka@gmail.com>
2018-03-10 17:20:53 +0100

    Add case for handling failed login attempts Add case for handling loading

--- client/reducers/sessionReducer.js
+++ client/reducers/sessionReducer.js
@@ -8,11 +8,26 @@ export default function sessionReducer(state = initialState.session, action) {
   switch (action.type) {
     case types.LOGIN_SUCCESS:
       history.push('/');
-      return {
-        jwt: !!sessionStorage.jwt,
-        isAdmin: !!sessionStorage.isAdmin,
-        userId: !!sessionStorage.userId
-      };
+      return (Object.assign(
+        state,
+        { jwt: !!sessionStorage.jwt },
+        { isAdmin: !!sessionStorage.isAdmin },
+        { userId: !!sessionStorage.userId },
+        { message: action.response.data.message },
+        { isLoading: false }
+      ));
+    case types.LOGIN_FAILURE:
+      history.push('/login');
+      return (Object.assign(
+        state,
+        { message: action.response.data.message },
+        { isLoading: false }
+      ));
+    case types.LOGIN_REQUEST:
+      return (Object.assign(
+        state,
+        { isLoading: true }
+      ));
     case types.LOGOUT_SUCCESS:
       history.push('/');
       return {
