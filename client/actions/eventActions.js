246ec317ec16636e8256a7a62a88f2e5bd9699d0
Okereke Onyekachi <robocopkaka@gmail.com>
2018-03-12 18:14:43 +0100

    Create action that is dispatched when there's an error updating an event Create action that's dispatched when a request has been sent to update an event

--- client/actions/eventActions.js
+++ client/actions/eventActions.js
@@ -51,7 +51,6 @@ export function addEvent(eventObject) {
         return response.data.message;
       })
       .catch((error) => {
-        console.log(error);
         dispatch(addEventFailure(error));
         throw error.data.message;
       });
@@ -61,14 +60,23 @@ export function addEvent(eventObject) {
 export function updateEventSuccess(event) {
   return { type: types.UPDATE_EVENT_SUCCESS, event };
 }
+export function updateEventFailure(event) {
+  return { type: types.UPDATE_EVENT_FAILURE, event };
+}
+export function updateEventLoading() {
+  return { type: types.UPDATE_EVENT_LOADING };
+}
 export function updateEvent(eventObject) {
-  return function (dispatch) {
+  return (dispatch) => {
+    dispatch(updateEventLoading());
     return EventApi.update(eventObject)
       .then((response) => {
-        dispatch(updateEventSuccess(response.data.event));
+        dispatch(updateEventSuccess(response.data));
+        return response.data.message;
       })
       .catch((error) => {
         console.log(error);
+        throw error.data.message;
       });
   };
 }
