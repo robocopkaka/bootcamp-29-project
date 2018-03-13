d0e3c579832e58ca97dc22e0b5d3e4565c65e4cf
Okereke Onyekachi <robocopkaka@gmail.com>
2018-03-13 23:42:05 +0100

    Show a spinning loader while centers are being fetched from the database

--- client/components/common/Preloader.jsx
+++ client/components/common/Preloader.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import CircularProgress from 'material-ui/CircularProgress';
+import * as styles from '../../css/centers.module.css';
 
 const Preloader = () => (
-  <CircularProgress size={150} thickness={5} />
+  <div className="min-height-hundred-vh">
+    <div className={styles['center-loader']}>
+      <CircularProgress size={80} thickness={5} />
+    </div>
+  </div>
 );
 export default Preloader;
