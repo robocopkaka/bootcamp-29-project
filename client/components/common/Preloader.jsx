69569ae66528f7611b08396b8552d479b47a7fec
Okereke Onyekachi <robocopkaka@gmail.com>
2018-03-13 14:36:18 +0100

    Use material ui's CircularProgress in Preloader component

--- client/components/common/Preloader.jsx
+++ client/components/common/Preloader.jsx
@@ -1,20 +1,7 @@
 import React from 'react';
+import CircularProgress from 'material-ui/CircularProgress';
 
 const Preloader = () => (
-  <div className="loader">
-    <div className="preloader-wrapper big active">
-      <div className="spinner-layer spinner-green-only">
-        <div className="circle-clipper left">
-          <div className="circle" />
-        </div>
-        <div className="gap-patch">
-          <div className="circle" />
-        </div>
-        <div className="circle-clipper right">
-          <div className="circle" />
-        </div>
-      </div>
-    </div>
-  </div>
+  <CircularProgress size={150} thickness={5} />
 );
 export default Preloader;
