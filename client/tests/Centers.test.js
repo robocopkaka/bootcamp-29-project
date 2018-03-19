1fffa82da96b547cf5730cf04aeb90db8474e5d7
Okereke Onyekachi <robocopkaka@gmail.com>
2018-03-19 17:25:24 +0100

    Fix tests for the Centers components

--- client/tests/Centers.test.js
+++ client/tests/Centers.test.js
@@ -1,20 +1,35 @@
-// import React from 'react';
-// import { shallow, configure } from 'enzyme';
-// import { expect } from 'chai';
-// import Adapter from 'enzyme-adapter-react-16';
-// import Centers from '../components/Centers';
-//
-// configure({ adapter: new Adapter() });
-//
-// describe('<Centers />', () => {
-//   let wrapper;
-//   beforeEach(() => {
-//     wrapper = shallow(<Centers />);
-//   });
-//   it('should have a CenterList component in it', () => {
-//     expect(wrapper.find('<CenterList />').length).to.equal(1);
-//   });
-//   it('should have a Search component in it', () => {
-//     expect(wrapper.find('<Search />').length).to.equal(1);
-//   });
-// });
+import React from 'react';
+import { shallow, configure } from 'enzyme';
+import { expect } from 'chai';
+import Adapter from 'enzyme-adapter-react-16';
+import { Centers } from '../components/centers/containers/Centers';
+import CenterList from '../components/centers/presentational/CenterList';
+import Search from '../components/common/Search';
+
+configure({ adapter: new Adapter() });
+
+describe('<Centers />', () => {
+  let wrapper;
+  const centers = [];
+  const isAdmin = false;
+  const isLoading = false;
+  const message = '';
+  const centerActions = {
+    fetchCenters: () => {}
+  };
+  beforeEach(() => {
+    wrapper = shallow(<Centers
+      centers={centers}
+      isAdmin={isAdmin}
+      isLoading={isLoading}
+      message={message}
+      centerActions={centerActions}
+    />);
+  });
+  it('should have a CenterList component in it', () => {
+    expect(wrapper.find(CenterList).length).to.equal(1);
+  });
+  it('should have a Search component in it', () => {
+    expect(wrapper.find(Search).length).to.equal(1);
+  });
+});
