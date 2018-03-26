4d237cf41dddbaa5572193cd7e8c316c3013003f
Okereke Onyekachi <robocopkaka@gmail.com>
2018-03-26 15:23:01 +0100

    Modify tests for the CenterList component

--- client/tests/AdminProfile.test.js
+++ client/tests/AdminProfile.test.js
@@ -1,33 +1,49 @@
 // import React from 'react';
-// import { mount, configure } from 'enzyme';
+// import { shallow, configure, mount } from 'enzyme';
 // import { expect } from 'chai';
-//
+// import { spy } from 'sinon';
 // import Adapter from 'enzyme-adapter-react-16';
-// import AdminProfile from '../components/AdminProfile';
-// import CenterList from '../components/CenterList';
-// import Search from '../components/Search';
-// import EventsListWithImage from '../components/events/EventsListWithImage';
+// import { AdminProfile } from '../components/Profiles/AdminProfile';
+// import CenterList from '../components/centers/presentational/CenterList';
+// import Search from '../components/common/Search';
+// import EventsListWithImage from '../components/events/presentational/EventsListWithImage';
 //
 // configure({ adapter: new Adapter() });
 //
 // describe('<AdminProfile />', () => {
 //   let wrapper;
+//   const centers = [];
+//   const events = [];
+//   const isAdmin = false;
+//   const centersLoading = false;
+//   const eventsLoading = false;
+//
 //   beforeEach(() => {
-//     wrapper = mount(<AdminProfile />);
+//     wrapper = shallow(<AdminProfile
+//       centers={centers}
+//       events={events}
+//       isAdmin={isAdmin}
+//       centersLoading={centersLoading}
+//       eventsLoading={eventsLoading}
+//     />);
+//     const componentDidMountSpy = spy(AdminProfile.prototype, 'componentDidMount');
 //   });
 //   it('should have a div with an all-events ID', () => {
-//     expect(wrapper.find('#all-events').length).to.equal(1);
+//     const componentDidMountSpy = spy(AdminProfile.prototype, 'componentDidMount');
+//     expect(AdminProfile.prototype.componentDidMount.calledOnce).to.equal(true);
+//     // expect(wrapper.find('#all-events').length).to.equal(1);
+//     componentDidMountSpy.restore();
 //   });
 //   it('should have a div with an all-centers ID', () => {
 //     expect(wrapper.find('#all-centers').length).to.equal(1);
 //   });
 //   it('should render a CenterList component', () => {
-//     expect(wrapper.find(<CenterList />).length).to.equal(1);
+//     expect(wrapper.find(CenterList).length).to.equal(1);
 //   });
 //   it('should render a EventsListWithImage component', () => {
-//     expect(wrapper.find(<EventsListWithImage />).length).to.equal(1);
+//     expect(wrapper.find(EventsListWithImage).length).to.equal(1);
 //   });
 //   it('should render a Search component twice', () => {
-//     expect(wrapper.find(<Search />).length).to.equal(2);
+//     expect(wrapper.find(Search).length).to.equal(2);
 //   });
 // });
--- client/tests/CenterList.test.js
+++ client/tests/CenterList.test.js
@@ -1,23 +1,37 @@
-// import React from 'react';
-// import { shallow, configure } from 'enzyme';
-// import { expect } from 'chai';
-// import Adapter from 'enzyme-adapter-react-16';
-// import CenterList from '../components/CenterList';
-//
-// configure({ adapter: new Adapter() });
-//
-// describe('<Centers />', () => {
-//   let wrapper;
-//   beforeEach(() => {
-//     wrapper = shallow(<CenterList />);
-//   });
-//   it('should have a div with a .container class', () => {
-//     expect(wrapper.find('.container').length).to.equal(1);
-//   });
-//   it('should have a div with a .card class', () => {
-//     expect(wrapper.find('.card').length).to.equal(1);
-//   });
-//   it('should have three divs inside the .card div', () => {
-//     expect(wrapper.find('.card').children().length).to.equal(3);
-//   });
-// });
+import React from 'react';
+import { shallow, configure } from 'enzyme';
+import { expect } from 'chai';
+import Adapter from 'enzyme-adapter-react-16';
+import CenterList from '../components/centers/presentational/CenterList';
+
+configure({ adapter: new Adapter() });
+
+describe('<Centers />', () => {
+  let wrapper;
+  const centers = [
+    {
+      id: 1,
+      name: 'name',
+      detail: 'detail',
+      address: 'address',
+      state: 'state',
+      capacity: 1000,
+      chairs: 10,
+      projector: 10,
+      image: 'stuff'
+    }
+  ];
+  const isAdmin = false;
+  beforeEach(() => {
+    wrapper = shallow(<CenterList
+      centers={centers}
+      isAdmin={isAdmin}
+    />);
+  });
+  it('should have a div with a .card class', () => {
+    expect(wrapper.find('.card').length).to.equal(1);
+  });
+  it('should have three divs inside the .card div', () => {
+    expect(wrapper.find('.card').children().length).to.equal(3);
+  });
+});
