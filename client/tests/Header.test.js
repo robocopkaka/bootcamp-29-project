b21e0a9b3b636a7b3417515a41daddab00bd36ab
Okereke Onyekachi <robocopkaka@gmail.com>
2018-03-26 15:28:17 +0100

    Modify tests for Header component

--- client/tests/Header.test.js
+++ client/tests/Header.test.js
@@ -1,34 +1,39 @@
-// import React from 'react';
-// import { shallow, configure } from 'enzyme';
-// import { expect } from 'chai';
-// import Adapter from 'enzyme-adapter-react-16';
-// import Header from '../components/Header';
-//
-// configure({ adapter: new Adapter() });
-//
-// describe('<Header />', () => {
-//   let wrapper;
-//   beforeEach(() => {
-//     wrapper = shallow(<Header />);
-//   });
-//   it('should have a nav element', () => {
-//     expect(wrapper.find('nav').length).to.equal(1);
-//   });
-//   it('should have an unordered list in the nav element with an ID - nav-mobile', () => {
-//     expect(wrapper.find('nav').children().find('#nav-mobile').length).to.equal(1);
-//   });
-//   it('should have four list elements in the unordered list with ID nav-mobile', () => {
-//     expect(wrapper.find('nav').children().find('#nav-mobile').children().length).to.equal(4);
-//   });
-//   it('should have an unordered list in the nav element with a class - side-nav', () => {
-//     expect(wrapper.find('nav').children().find('.side-nav').length).to.equal(1);
-//   });
-//   it('should have four list elements in the unordered list with class side-nav', () => {
-//     expect(wrapper.find('nav').children().find('.side-nav').children().length).to.equal(4);
-//   });
-//   it('should have an unordered list inside a list element in the unordered list with a .side-nav class', () => {
-//     expect(wrapper.find('nav').children().find('.side-nav').children()
-//       .find('.left-padding')
-//       .children().length).to.equal(1);
-//   });
-// });
+import React from 'react';
+import { shallow, configure } from 'enzyme';
+import { expect } from 'chai';
+import Adapter from 'enzyme-adapter-react-16';
+import { Header } from '../components/common/Header';
+
+configure({ adapter: new Adapter() });
+
+describe('<Header />', () => {
+  let wrapper;
+  const loggedIn = false;
+  const isAdmin = false;
+  beforeEach(() => {
+    wrapper = shallow(<Header
+      loggedIn={loggedIn}
+      isAdmin={isAdmin}
+    />);
+  });
+  it('should have a nav element', () => {
+    expect(wrapper.find('nav').length).to.equal(1);
+  });
+  it('should have an unordered list in the nav element with an ID - nav-mobile', () => {
+    expect(wrapper.find('nav').children().find('#nav-mobile').length).to.equal(1);
+  });
+  it('should have four list elements in the unordered list with ID nav-mobile', () => {
+    expect(wrapper.find('nav').children().find('#nav-mobile').children().length).to.equal(4);
+  });
+  it('should have an unordered list in the nav element with a class - side-nav', () => {
+    expect(wrapper.find('nav').children().find('.side-nav').length).to.equal(1);
+  });
+  it('should have four list elements in the unordered list with class side-nav', () => {
+    expect(wrapper.find('nav').children().find('.side-nav').children().length).to.equal(4);
+  });
+  it('should have an unordered list inside a list element in the unordered list with a .side-nav class', () => {
+    expect(wrapper.find('nav').children().find('.side-nav').children()
+      .find('.left-padding')
+      .children().length).to.equal(1);
+  });
+});
