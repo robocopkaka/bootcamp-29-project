44365489fc2b3bab93675b662a47b07c4e4b54eb
Okereke Onyekachi <robocopkaka@gmail.com>
2018-04-01 10:14:54 +0100

    Redesign single event page
    Remove card
    Put center image and some info about the event in grids
    Put event details under the grid

--- client/components/events/presentational/EventDetails.jsx
+++ client/components/events/presentational/EventDetails.jsx
@@ -5,23 +5,41 @@ import * as eventStyles from '../../../css/events.module.css';
 import * as centerStyles from '../../../css/centers.module.css';
 
 const EventDetails = ({ event, center }) => (
-  <div className="col s12 m8 l8">
-    <div className="row">
-      <div className="col s12 m8 l8">
-        <div className="card">
-          <div className="card-image">
-            <img src={center.image} alt="" className="responsive-img" />
-            <span className="card-title">{event.name}</span>
+  <div className="single-event-container">
+    <div className="row" id={eventStyles['single-event-row']}>
+      <div>
+        <img src={center.image} alt="" className="responsive-img col s12 m12 l8 large-event-image" />
+      </div>
+      <div className="col s12 m12 l4" id={eventStyles['event-details-padding']}>
+        <h1 id={eventStyles['break-word']}>{event.name}</h1>
+        <React.Fragment>
+          <p className={eventStyles.tiny}>
+            <span className={eventStyles['tiny-span']}>Date</span>
+          </p>
+          <p className={eventStyles['show-event-details']}>{moment(event.date).format('LL')}</p>
+        </React.Fragment>
+        <div>
+          <div className="show-event-time">
+            <p className={eventStyles.tiny}>
+              <span className={eventStyles['tiny-span']}>Time</span>
+            </p>
+            <p className={eventStyles['show-event-details']}>{moment(event.date).format('LT')}</p>
           </div>
-          <div className="card-content">
-            <p id={centerStyles['show-center-address']}>{event.detail}</p><br />
-            <span id={centerStyles['show-center-state']}>
-              Venue: {center.address}, {center.state}
-            </span><br />
-            <span className={eventStyles['event-date']}>{moment(event.date).format('LT')}</span><br />
-            <span className={eventStyles['event-date']}>Guests: {event.guests}</span>
+          <div>
+            <p className={eventStyles.tiny}>
+              <span className={eventStyles['tiny-span']}>Guest</span>
+            </p>
+            <p className={eventStyles['show-event-details']}>{event.guests}</p>
           </div>
         </div>
+      </div>
+    </div>
+    <div className={eventStyles['single-event-details']}>
+      <div>
+        <h2 className={eventStyles['event-detail']}>
+          <span id={eventStyles['event-detail-span']}>Event Details</span>
+        </h2>
+        <p id={eventStyles['event-detail-text']}>{event.detail}</p>
       </div>
     </div>
   </div>
