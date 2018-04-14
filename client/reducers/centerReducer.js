c6d224d99f8488493791e8e3669cc4eef40e2740
Okereke Onyekachi <robocopkaka@gmail.com>
2018-04-15 00:39:48 +0100

    Remove Object.assign calls from centerReducer

--- client/reducers/centerReducer.js
+++ client/reducers/centerReducer.js
@@ -2,35 +2,43 @@ import update from 'immutability-helper';
 import initialState from './initialState';
 import * as types from '../actions/actionTypes';
 
+function addCenterReducer(state = [], action) {
+  let newState = {};
+  switch (action.type) {
+    case types.ADD_CENTER_SUCCESS:
+      newState = update(state, {
+        $set: [
+          ...state.filter(center => center.id !== action.center.center.id),
+          Object.assign({}, action.center.center)
+        ]
+      });
+      return newState;
+    default:
+      return state;
+  }
+}
+
 export default function centerReducer(state = initialState.centers, action) {
   let newState = {};
   switch (action.type) {
     case types.ADD_CENTER_SUCCESS:
-      return (Object.assign(
-        {},
-        state,
-        {
-          centers: [
-            ...state.centers.filter(center => center.id !== action.center.center.id),
-            Object.assign({}, action.center.center)
-          ]
-        },
-        { isLoading: false },
-        { message: action.center.message }
-      ));
+      newState = update(state, {
+        centers: { $set: addCenterReducer(state.centers, action) },
+        isLoading: { $set: false },
+        message: { $set: action.center.message }
+      });
+      return newState;
     case types.ADD_CENTER_FAILURE:
-      return (Object.assign(
-        {},
-        state,
-        { isLoading: false },
-        { message: action.center.data.message }
-      ));
+      newState = update(state, {
+        isLoading: { $set: false },
+        message: { $set: action.center.data.message }
+      });
+      return newState;
     case types.ADD_CENTER_LOADING:
-      return (Object.assign(
-        {},
-        state,
-        { isLoading: true }
-      ));
+      newState = update(state, {
+        isLoading: { $set: true }
+      });
+      return newState;
     case types.FETCH_CENTERS_SUCCESS:
       newState = update(state, {
         centers: {
@@ -42,43 +50,38 @@ export default function centerReducer(state = initialState.centers, action) {
       });
       return newState;
     case types.FETCH_CENTERS_FAILURE:
-      return (Object.assign(
-        {},
-        state,
-        { isLoading: false },
-        // { message: action.centers.message }
-      ));
+      newState = update(state, {
+        isLoading: { $set: false }
+      });
+      return newState;
     case types.FETCH_CENTERS_LOADING:
-      return (Object.assign(
-        {},
-        { isLoading: true },
-      ));
+      newState = update(state, {
+        isLoading: { $set: true }
+      });
+      return newState;
     case types.UPDATE_CENTER_SUCCESS:
-      return (Object.assign(
-        {},
-        state,
-        {
-          centers: [
+      newState = update(state, {
+        centers: {
+          $set: [
             ...state.centers.filter(center => center.id !== action.center.center.id),
             Object.assign({}, action.center.center)
           ]
         },
-        { isLoading: false },
-        { message: action.center.message }
-      ));
+        isLoading: { $set: false },
+        message: { $set: action.center.message }
+      });
+      return newState;
     case types.UPDATE_CENTER_FAILURE:
-      return (Object.assign(
-        {},
-        state,
-        { isLoading: false },
-        { message: action.center.data.message }
-      ));
+      newState = update(state, {
+        isLoading: { $set: false },
+        message: { $set: action.center.data.message }
+      });
+      return newState;
     case types.UPDATE_CENTER_LOADING:
-      return (Object.assign(
-        {},
-        state,
-        { isLoading: true }
-      ));
+      newState = update(state, {
+        isLoading: { $set: true }
+      });
+      return newState;
     default:
       return state;
   }
