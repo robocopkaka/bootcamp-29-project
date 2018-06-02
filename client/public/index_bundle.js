8d6cb3af9ae6e23572f7a987625c78e371fe2807
Onyekachi Okereke <robocopkaka@gmail.com>
2018-06-02 18:55:17 +0100

    chore(heroku): deploy to heroku
    move webpack-merge to devDepencies
    
    [#158025935]

--- client/public/index_bundle.js
+++ client/public/index_bundle.js
@@ -61,7 +61,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "8013cb93a774809438fc"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "73b02207094c3623c82f"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
