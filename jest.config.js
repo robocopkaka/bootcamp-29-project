17ef149759d9011600713389159d1c1b4bf34bae
Onyekachi Okereke <robocopkaka@gmail.com>
2018-06-07 08:43:50 +0100

    chore(e2e): add end to end testing
    untrack e2e tests when running jest --coverage
    
    [#158111920]

--- jest.config.js
+++ jest.config.js
@@ -10,5 +10,6 @@ module.exports = {
     ],
     "testMatch": [ "**/client/tests/**/*.test.js?(x)", "**/?(*.)+(spec|test).js?(x)" ],
     "coveragePathIgnorePatterns": ["./client/index.js", "./client/history.js",
-    "./client/public/index_bundle.js", "./client/public/index_bundle.js.map", "./client/public/index.html"]
+    "./client/public/index_bundle.js", "./client/public/index_bundle.js.map", "./client/public/index.html"
+    ,"./client/e2eTests"]
 }
